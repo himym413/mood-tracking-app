b181710b905ce9383ea7f6838dfa0f7e35b4a958
himym413 <igorseslak7@gmail.com>
2025-11-04 18:58:23 +0100

    Fixed button accessibility, added chart auto scroll to right on smaller devices

--- src/features/chart/CustomTooltip.tsx
+++ src/features/chart/CustomTooltip.tsx
@@ -28,24 +28,30 @@ function CustomTooltip({ active, payload }: CustomTooltipProps) {
 
   if (active && payload && payload.length > 0) {
     return (
-      <div className="w-[175px] bg-neutral-0 p-150 flex flex-col items-start gap-150 rounded-10 shadow-[0_4px_7px_rgba(33,33,77,0.16)]">
+      <div className="w-[150px] md:w-[175px] bg-neutral-0 p-100 md:p-150 flex flex-col items-start gap-100 md:gap-150 rounded-10 shadow-[0_4px_7px_rgba(33,33,77,0.16)]">
         <div>
-          <h6 className="text-preset-8 text-neutral-600 mb-075">Mood</h6>
+          <h6 className="text-preset-8 text-neutral-600 mb-050 md:mb-075">
+            Mood
+          </h6>
           <div className="flex items-center gap-075">
             <img src={imageSource} className="w-200 h-200" />
             <p className="text-preset-7 text-neutral-900">{moodText}</p>
           </div>
         </div>
         <div>
-          <h6 className="text-preset-8 text-neutral-600 mb-075">Sleep</h6>
+          <h6 className="text-preset-8 text-neutral-600 mb-050 md:mb-075">
+            Sleep
+          </h6>
           <p className="text-preset-7 text-neutral-900">{sleepText}</p>
         </div>
         <div>
-          <h6 className="text-preset-8 text-neutral-600 mb-075">Reflection</h6>
+          <h6 className="text-preset-8 text-neutral-600 mb-050 md:mb-075">
+            Reflection
+          </h6>
           <p className="text-preset-9 text-neutral-900">{journalEntry}</p>
         </div>
         <div>
-          <h6 className="text-preset-8 text-neutral-600 mb-075">Tags</h6>
+          <h6 className="text-preset-8 text-neutral-600 md:mb-075">Tags</h6>
           {feelings.map((feeling, i, arr) => (
             <span key={feeling} className="text-preset-9 text-neutral-900">
               {feeling}
